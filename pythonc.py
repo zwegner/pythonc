979d7c578ff88f855ab384bacfd0b2d5e0386765
Zach Wegner <zwegner@gmail.com>
2011-08-13 15:36:33 -0700

    Add an annoying timer for the program in pythonc.py ---  pythonc.py |    4 ++++  1 files changed, 4 insertions(+), 0 deletions(-)

--- pythonc.py
+++ pythonc.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import sys
+import time
 
 def usage():
     print('usage: %s [-O] <input.py> [args...]' % sys.argv[0])
@@ -23,4 +24,7 @@ base = os.path.splitext(path)[0]
 
 subprocess.check_call(['python3', 'transform.py', path, '%s.cpp' % base])
 subprocess.check_call(['c++'] + gcc_flags + ['%s.cpp' % base, '-o', base])
+start = time.time()
 subprocess.check_call(['./%s' % base] + args[1:])
+elapsed = time.time() - start
+print('Total time: %ss' % elapsed)
