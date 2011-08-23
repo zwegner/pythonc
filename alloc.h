898f50e4e9a7a2ed66cd0455eb182e9b3ee3f1a0
Zach Wegner <zwegner@gmail.com>
2011-08-22 22:15:24 -0700

    Remove crud from using the allocator as an STL backend ---  alloc.h |   62 --------------------------------------------------------------  1 files changed, 0 insertions(+), 62 deletions(-)

--- alloc.h
+++ alloc.h
@@ -124,68 +124,6 @@ public:
 
 arena allocator[1];
 
-template <class T>
-class alloc;
-
-template <>
-class alloc<void> {
-public:
-    typedef void *pointer;
-    typedef const void *const_pointer;
-    typedef void value_type;
-    template <class U> struct rebind {
-        typedef alloc<U> other;
-    };
-};
-
-template <class T>
-class alloc {
-public:
-    typedef T value_type;
-    typedef T *pointer;
-    typedef const T *const_pointer;
-    typedef ptrdiff_t difference_type;
-    typedef T &reference;
-    typedef const T &const_reference;
-    typedef size_t size_type;
-    template <class U>
-    struct rebind {
-        typedef alloc<U> other;
-    };
-
-    alloc() {}
-    alloc(const alloc &x) {}
-    template <class U>
-    alloc(const alloc<U> &x) {}
-    ~alloc() {}
-
-    T *address(T &x) const { return &x; }
-    const T* address(const T &x) const { return &x; }
-
-    T *allocate(size_t n, alloc<void>::const_pointer hint=0) {
-        return (T *)allocator->allocate(n * sizeof(T));
-    }
-    void deallocate(T *p, size_t n) {
-        allocator->deallocate(p, n * sizeof(T));
-    }
-    size_t max_size() const {
-        return size_t(-1) / sizeof(value_type);
-    }
-
-    void construct(T *p, const T &val) { new(static_cast<void *>(p)) T(val); }
-    void destroy(T *p) { p->~T(); }
-};
-
-template <typename T, typename U>
-bool operator==(const alloc<T>&, const alloc<U>) {
-    return true;
-}
-
-template <typename T, typename U>
-bool operator!=(const alloc<T>&, const alloc<U>) {
-    return false;
-}
-
 void *operator new(size_t bytes, arena *a) {
     return a->allocate(bytes);
 }
