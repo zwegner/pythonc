9cc37c56582ede400140accce9dc07a6777e66e2
Zach Wegner <zwegner@gmail.com>
2011-10-26 23:59:38 -0700

    Better test support ---  test.py |   18 ++++++++++++++++--  1 files changed, 16 insertions(+), 2 deletions(-)  mode change 100644 => 100755 test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 import os
 import shutil
 import subprocess
+import time
 
 # XXX This file is really stupid!! Just a 5-minute effort on my part...
 
@@ -10,5 +12,17 @@ for i in ['pythonc.py', 'syntax.py', 'transform.py', 'backend.cpp', 'alloc.h']:
     shutil.copy(i, '.temp')
 
 for i in os.listdir('tests'):
-    shutil.copy('tests/%s' % i, '.temp/test.py')
-    subprocess.check_call('./pythonc.py test.py', cwd='.temp', shell=True)
+    if i.endswith('.py'):
+        shutil.copy('tests/%s' % i, '.temp/test.py')
+        start = time.time()
+        out_p = subprocess.check_output('./pythonc.py test.py', cwd='.temp', shell=True)
+        mid = time.time()
+        out_c = subprocess.check_output('python3 test.py', cwd='.temp', shell=True)
+        end = time.time()
+        if out_p != out_c:
+            raise RuntimeError('%s mismatched!\nPythonc:\n%s\nCPython:\n%s' % (i,
+                out_p.decode(), out_c.decode()))
+        time_p = mid - start
+        time_c = end - mid
+        # XXX timing should factor out translation time
+        print('%s: pythonc=%ss cpython=%ss (%.3fx)' % (i, time_p, time_c, time_p / time_c))
