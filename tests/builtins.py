b84eec0c49df814bc00114eab910fd0adf4f5029
Matt Craighead <mjcraighead@gmail.com>
2011-10-27 22:55:51 -0700

    more tests ---  tests/builtins.py |   31 +++++++++++++++++++++++++++++++  1 files changed, 31 insertions(+), 0 deletions(-)

--- tests/builtins.py
+++ tests/builtins.py
@@ -1,2 +1,33 @@
 for x in [bool, dict, int, isinstance, len, list, open, ord, print, range, set, sorted, str]:
     print(x)
+
+for x in [bool, dict, int, list, set, str]:
+    print(x())
+
+for x in [0, 1]:
+    print(bool(x))
+
+for x in [0, 1, '0', '1', '10', '15']:
+    print(int(x))
+
+for x in ['', [], {}, set(), 'a', [1], {'b'}, {1: 2}]:
+    print(len(x))
+
+for x in [range(5), {1, 2, 3}, [1, 'c']]:
+    print(list(x))
+
+for x in ['a', ' ', '+']:
+    print(ord(x))
+
+for x in [0, 1, 5, 10]:
+    print(range(x))
+    print(list(range(x)))
+
+for x in [[1]]:
+    print(set(x))
+
+for x in [[5, 3, 1]]:
+    print(sorted(x))
+
+for x in [0, 'a', []]:
+    print(str(x))
