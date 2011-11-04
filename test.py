0e8484710188220897ae080fca7dcc2a86b51676
Zach Wegner <zwegner@gmail.com>
2011-11-04 02:31:32 +0000

    Allow test.py to take a list of tests to run, and add a silly math test git-svn-id: http://svn.code.sf.net/p/pythonc/code/trunk@6 ee257a29-4c5a-45b2-9e25-670a909b6866 ---  test.py         |   15 ++++++++++++---  tests/kernel.py |    7 +++++++  2 files changed, 19 insertions(+), 3 deletions(-)  create mode 100644 tests/kernel.py

--- test.py
+++ test.py
@@ -3,16 +3,20 @@ import os
 import shutil
 import subprocess
 import time
+import sys
 
-# XXX This file is really stupid!! Just a 5-minute effort on my part...
+tests = set('%s.py' % s for s in sys.argv[1:])
 
 if not os.path.exists('.temp'):
     os.mkdir('.temp')
 for i in ['pythonc.py', 'syntax.py', 'transform.py', 'backend.cpp', 'alloc.h']:
     shutil.copy(i, '.temp')
 
+passes = 0
+fails = 0
+
 for i in os.listdir('tests'):
-    if i.endswith('.py'):
+    if i.endswith('.py') and (not tests or i in tests):
         shutil.copy('tests/%s' % i, '.temp/test.py')
         comp_start = time.time()
         out_p = subprocess.check_output('./pythonc.py -O -q -c test.py', cwd='.temp', shell=True)
@@ -22,9 +26,14 @@ for i in os.listdir('tests'):
         out_c = subprocess.check_output('python3 test.py', cwd='.temp', shell=True)
         end = time.time()
         if out_p != out_c:
-            raise RuntimeError('%s mismatched!\nPythonc:\n%s\nCPython:\n%s' % (i,
+            print('%s mismatched!\nPythonc:\n%s\nCPython:\n%s' % (i,
                 out_p.decode(), out_c.decode()))
+            fails += 1
+        else:
+            passes += 1
         time_compile = start - comp_start
         time_p = mid - start
         time_c = end - mid
         print('%s: pythonc=%.3fs (compile=%.3fs) cpython=%.3fs (%.3fx)' % (i, time_p, time_compile, time_c, time_c / time_p))
+
+print('%s/%s tests passed.' % (passes, passes + fails))
---
+++ tests/kernel.py
@@ -0,0 +1,7 @@
+x = 0
+y = 4
+while x < 1000000:
+    x += 1
+    y ^= y * 7
+    y &= 0xFFFFFFFF
+print(x, y)
