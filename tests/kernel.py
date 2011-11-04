8d61aa1b78b10ade9b84bcef0136278511d7293d
Zach Wegner <zwegner@gmail.com>
2011-11-04 02:47:10 +0000

    Make tests/kernel.py a bit sillier/stressful git-svn-id: http://svn.code.sf.net/p/pythonc/code/trunk@8 ee257a29-4c5a-45b2-9e25-670a909b6866 ---  tests/kernel.py |   10 +++++++++-  1 files changed, 9 insertions(+), 1 deletions(-)

--- tests/kernel.py
+++ tests/kernel.py
@@ -2,6 +2,14 @@ x = 0
 y = 4
 while x < 1000000:
     x += 1
-    y ^= y * 7
+    y |= y * 7
+    y &= 0xFFFFFFFF
+print(x, y)
+
+x = 0
+y = 4
+while x < 1000000:
+    x += 1
+    y = {x: y | y * 7}[x]
     y &= 0xFFFFFFFF
 print(x, y)
