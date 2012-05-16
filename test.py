aea41a7ef587525df6e3220b39fe8aa4633d036e
Zach Wegner <zwegner@gmail.com>
2012-05-16 05:06:19 -0700

    Improve test.py a bit, allow for test directories with multiple files

--- test.py
+++ test.py
@@ -5,35 +5,54 @@ import subprocess
 import time
 import sys
 
-tests = set('%s.py' % s for s in sys.argv[1:])
-
-if not os.path.exists('.temp'):
-    os.mkdir('.temp')
-for i in ['pythonc.py', 'syntax.py', 'transform.py', 'backend.cpp', 'alloc.py']:
-    shutil.copy(i, '.temp')
+tests = set(sys.argv[1:])
 
 passes = 0
 fails = 0
 
 for i in os.listdir('tests'):
-    if i.endswith('.py') and (not tests or i in tests):
-        shutil.copy('tests/%s' % i, '.temp/test.py')
-        comp_start = time.time()
-        out_p = subprocess.check_output('./pythonc.py -O -q -c test.py', cwd='.temp', shell=True)
-        start = time.time()
-        out_p = subprocess.check_output('./test', cwd='.temp', shell=True)
-        mid = time.time()
-        out_c = subprocess.check_output('python3 test.py', cwd='.temp', shell=True)
-        end = time.time()
-        if out_p != out_c:
-            print('%s mismatched!\nPythonc:\n%s\nCPython:\n%s' % (i,
-                out_p.decode(), out_c.decode()))
-            fails += 1
-        else:
-            passes += 1
-        time_compile = start - comp_start
-        time_p = mid - start
-        time_c = end - mid
-        print('%s: pythonc=%.3fs (compile=%.3fs) cpython=%.3fs (%.3fx)' % (i, time_p, time_compile, time_c, time_c / time_p))
+    # Check for test filter
+    if tests and i.replace('.py', '') not in tests:
+        continue
+
+    # Clear the temp directory
+    cwd = '.temp'
+    shutil.rmtree(cwd)
+    os.mkdir(cwd)
+    for j in ['pythonc.py', 'syntax.py', 'transform.py', 'backend.cpp', 'alloc.py']:
+        shutil.copy(j, cwd)
+
+    path = 'tests/%s' % i
+    if i.endswith('.py'):
+        shutil.copy(path, '%s/test.py' % cwd)
+    elif os.path.isdir(path):
+        for j in os.listdir(path):
+            shutil.copy('%s/%s' % (path, j), cwd)
+    else:
+        continue
+
+    # Compile
+    comp_start = time.time()
+    out_p = subprocess.check_output('./pythonc.py -O -q -c test.py', cwd=cwd, shell=True)
+
+    # Run pythonc-compiled version
+    start = time.time()
+    out_p = subprocess.check_output('./test', cwd=cwd, shell=True)
+
+    # Run CPython version
+    mid = time.time()
+    out_c = subprocess.check_output('python3 test.py', cwd=cwd, shell=True)
+    end = time.time()
+
+    if out_p != out_c:
+        print('%s mismatched!\nPythonc:\n%s\nCPython:\n%s' % (i,
+            out_p.decode(), out_c.decode()))
+        fails += 1
+    else:
+        passes += 1
+    time_compile = start - comp_start
+    time_p = mid - start
+    time_c = end - mid
+    print('%s: pythonc=%.3fs (compile=%.3fs) cpython=%.3fs (%.3fx)' % (i, time_p, time_compile, time_c, time_c / time_p))
 
 print('%s/%s tests passed.' % (passes, passes + fails))
